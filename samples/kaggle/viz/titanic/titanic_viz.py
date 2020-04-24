e9fedd8d62951f69be2135c47f0feefecde13944
Kindlytree <lsxu@nullmax.ai>
2020-04-23 22:41:06 -0400

    update kaggle viz sample

--- samples/kaggle/viz/titanic/titanic_viz.py
+++ samples/kaggle/viz/titanic/titanic_viz.py
@@ -35,5 +35,63 @@ plt.xlabel('Survived?');
 plt.ylabel('Number of occurrences');
 plt.show()
 
-import IPython
-IPython.embed(colors="Linux")
+
+# Getting correlation matrix
+'''DataFrame.corr(method=‘pearson’, min_periods=1)
+计算列与列之间的相关系数，返回相关系数矩阵
+pandas中可以使用round(n)方法返回 x 的小数点四舍五入到n个数字。简洁的说就是，四舍五入的保留小数点后的几个数字。round()不添加任何参数的时候，等同于round(0)就是取整。
+'''
+cor_matrix = train_data[numeric_features + ordinal_features].corr().round(2)
+# Plotting heatmap 
+fig = plt.figure(figsize=(12,12));
+sns.heatmap(cor_matrix, annot=True, center=0, cmap = sns.diverging_palette(250, 10, as_cmap=True), ax=plt.subplot(111));
+plt.show()
+
+
+### Plotting Numeric Features
+# Looping through and Plotting Numeric features
+for column in numeric_features:    
+    # Figure initiation
+    fig = plt.figure(figsize=(18,12))
+    
+    ### Distribution plot
+    sns.distplot(train_data[column].dropna(), ax=plt.subplot(221));
+    # X-axis Label
+    plt.xlabel(column, fontsize=14);
+    # Y-axis Label
+    plt.ylabel('Density', fontsize=14);
+    # Adding Super Title (One for a whole figure)
+    plt.suptitle('Plots for '+column, fontsize=18);
+    
+    ### Distribution per Survived / Not Survived Value
+    # Not Survived hist
+    sns.distplot(train_data.loc[train_data.Survived==0, column].dropna(),
+                 color='red', label='Not Survived', ax=plt.subplot(222));
+    # Survived hist
+    sns.distplot(train_data.loc[train_data.Survived==1, column].dropna(),
+                 color='blue', label='Survived', ax=plt.subplot(222));
+    # Adding Legend
+    plt.legend(loc='best')
+    # X-axis Label
+    plt.xlabel(column, fontsize=14);
+    # Y-axis Label
+    plt.ylabel('Density per Survived / Not Survived Value', fontsize=14);
+    
+    ### Average Column value per Survived / Not Survived Value
+    sns.barplot(x="target_name", y=column, data=train_data, ax=plt.subplot(223));
+    # X-axis Label
+    plt.xlabel('Survived or Not Survived?', fontsize=14);
+    # Y-axis Label
+    plt.ylabel('Average ' + column, fontsize=14);
+    
+    ### Boxplot of Column per Survived / Not Survived Value
+    sns.boxplot(x="target_name", y=column, data=train_data, ax=plt.subplot(224));
+    # X-axis Label
+    plt.xlabel('Survived or Not Survived?', fontsize=14);
+    # Y-axis Label
+    plt.ylabel(column, fontsize=14);
+    # Printing Chart
+    plt.show()
+   
+# import IPython
+# IPython.embed(colors="Linux")
